a5847c4c4353b861c802472db0040b0e528c39b5
Pete <pjuliana@ncmec.org>
2019-07-24 16:57:59 -0400

    Add Hero Service. Inject Hero Service into Hero Component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import {HEROES} from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,16 +9,22 @@ import {HEROES} from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
-  ngOnInit() {
-  }
+  heroes: Hero[];
 
-  heroes = HEROES;
   selectedHero: Hero;
 
-  onSelect(hero: Hero): void{
-    this.selectedHero=hero;
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  getHeroes() {
+    this.heroes = this.heroService.getHeroes();
   }
 
 }
